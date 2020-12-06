fbee91783b46a940aed36a2427627f5f4d30d0b6
Jevon Cochran <jevon.cochran@gmail.com>
2020-12-06 11:33:34 -0800

    fixed height for admin menu item cards

--- website/src/components/admin-view/AdminDash.js
+++ website/src/components/admin-view/AdminDash.js
@@ -32,31 +32,91 @@ const AdminDash = () => {
       });
   }, []);
 
+  const editDayAvailability = (newStatus, itemId) => {
+    axios
+      .patch(`http://localhost:5000/api/menu/item${itemId}`, newStatus)
+      .then((res) => {
+        console.log(res);
+        axios.get("http://localhost:5000/api/menu").then((res) => {
+          // console.log(res);
+          setMenu({
+            entrees: res.data.filter((item) => item.category === "entree"),
+            sides: res.data.filter((item) => item.category === "side"),
+            extras: res.data.filter((item) => item.category === "extra"),
+            dessert: res.data.filter(
+              (item) => item.dinner_or_dessert === "dessert"
+            ),
+          });
+        });
+      });
+  };
+
   return (
     <div>
       <h1 className="ad-headline">Admin Dashboard</h1>
-      <h2>Menu Options</h2>
+      <h2 style={{ textAlign: "center" }}>Menu Options</h2>
       {menu.entrees.length > 0 && (
         <div className="cur-menu-category-container">
-          <h3 className="cur-menu-label">Entrees</h3>
+          <h3 className="ad-menu-label">Entrees</h3>
           <Grid container spacing={2}>
             {menu.entrees.map((item) => (
               <Grid item xs={2}>
-                <Card className="cur-menu-card">
-                  <div className="cur-menu-deets-cont">
-                    <p className="cur-menu-item-name">{item.item}</p>
-                    <p className="cur-menu-item-description">
+                <Card className="ad-menu-item-card" style={{ height: "40vh" }}>
+                  <div className="ad-menu-item-deets-cont">
+                    <div className="ad-avail-status-div">
+                      <p
+                        className={
+                          item.available_today == 1
+                            ? "ad-avail-green"
+                            : "ad-avail-red"
+                        }
+                      >
+                        AVAILABLE TODAY
+                      </p>
+                    </div>
+                    <p className="ad-menu-item-name">{item.item}</p>
+                    {/* <p className="cur-menu-item-description">
                       {item.description}
-                    </p>
-                    <p className="cur-menu-item-price">
+                    </p> */}
+                    <p className="ad-menu-item-price">
                       {CurrencyFormatter.format(item.price, {
                         currency: "USD",
                       })}
                     </p>
                   </div>
-                  <div className="cur-menu-item-img">
+                  <div className="ad-menu-item-img">
                     <object data={item.image} alt="pic of menu item" />
                   </div>
+                  {!item.available_today && (
+                    <div className="ad-avail-btn-div">
+                      <button
+                        className="ad-mark-avail-btn"
+                        onClick={() =>
+                          editDayAvailability(
+                            { available_today: true },
+                            item.id
+                          )
+                        }
+                      >
+                        +
+                      </button>
+                    </div>
+                  )}
+                  {item.available_today === 1 && (
+                    <div className="ad-avail-btn-div">
+                      <button
+                        className="ad-mark-unavail-btn"
+                        onClick={() =>
+                          editDayAvailability(
+                            { available_today: false },
+                            item.id
+                          )
+                        }
+                      >
+                        x
+                      </button>
+                    </div>
+                  )}
                 </Card>
               </Grid>
             ))}
@@ -66,25 +126,135 @@ const AdminDash = () => {
 
       {menu.sides.length > 0 && (
         <div className="cur-menu-category-container">
-          <h3 className="cur-menu-label">Sides</h3>
+          <h3 className="ad-menu-label">Sides</h3>
           <Grid container spacing={2}>
             {menu.sides.map((item) => (
-              <Grid item xs={3}>
-                <Card className="cur-menu-card">
-                  <div className="cur-menu-deets-cont">
-                    <p className="cur-menu-item-name">{item.item}</p>
-                    <p className="cur-menu-item-description">
+              <Grid item xs={2}>
+                <Card className="ad-menu-item-card" style={{ height: "40vh" }}>
+                  <div className="ad-menu-item-deets-cont">
+                    <div className="ad-avail-status-div">
+                      <p
+                        className={
+                          item.available_today == 1
+                            ? "ad-avail-green"
+                            : "ad-avail-red"
+                        }
+                      >
+                        AVAILABLE TODAY
+                      </p>
+                    </div>
+                    <p className="ad-menu-item-name">{item.item}</p>
+                    {/* <p className="cur-menu-item-description">
                       {item.description}
-                    </p>
-                    <p className="cur-menu-item-price">
+                    </p> */}
+                    <p className="ad-menu-item-price">
                       {CurrencyFormatter.format(item.price, {
                         currency: "USD",
                       })}
                     </p>
                   </div>
-                  <div className="cur-menu-item-img">
+                  <div className="ad-menu-item-img">
                     <object data={item.image} alt="pic of menu item" />
                   </div>
+                  {!item.available_today && (
+                    <div className="ad-avail-btn-div">
+                      <button
+                        className="ad-mark-avail-btn"
+                        onClick={() =>
+                          editDayAvailability(
+                            { available_today: true },
+                            item.id
+                          )
+                        }
+                      >
+                        +
+                      </button>
+                    </div>
+                  )}
+                  {item.available_today === 1 && (
+                    <div className="ad-avail-btn-div">
+                      <button
+                        className="ad-mark-unavail-btn"
+                        onClick={() =>
+                          editDayAvailability(
+                            { available_today: false },
+                            item.id
+                          )
+                        }
+                      >
+                        x
+                      </button>
+                    </div>
+                  )}
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
+      )}
+
+      {menu.dessert.length > 0 && (
+        <div className="cur-menu-category-container">
+          <h3 className="ad-menu-label">Dessert</h3>
+          <Grid container spacing={2}>
+            {menu.dessert.map((item) => (
+              <Grid item xs={2}>
+                <Card className="ad-menu-item-card" style={{ height: "40vh" }}>
+                  <div className="ad-menu-item-deets-cont">
+                    <div className="ad-avail-status-div">
+                      <p
+                        className={
+                          item.available_today == 1
+                            ? "ad-avail-green"
+                            : "ad-avail-red"
+                        }
+                      >
+                        AVAILABLE TODAY
+                      </p>
+                    </div>
+                    <p className="ad-menu-item-name">{item.item}</p>
+                    {/* <p className="cur-menu-item-description">
+                      {item.description}
+                    </p> */}
+                    <p className="ad-menu-item-price">
+                      {CurrencyFormatter.format(item.price, {
+                        currency: "USD",
+                      })}
+                    </p>
+                  </div>
+                  <div className="ad-menu-item-img">
+                    <object data={item.image} alt="pic of menu item" />
+                  </div>
+                  {!item.available_today && (
+                    <div className="ad-avail-btn-div">
+                      <button
+                        className="ad-mark-avail-btn"
+                        onClick={() =>
+                          editDayAvailability(
+                            { available_today: true },
+                            item.id
+                          )
+                        }
+                      >
+                        +
+                      </button>
+                    </div>
+                  )}
+                  {item.available_today === 1 && (
+                    <div className="ad-avail-btn-div">
+                      <button
+                        className="ad-mark-unavail-btn"
+                        onClick={() =>
+                          editDayAvailability(
+                            { available_today: false },
+                            item.id
+                          )
+                        }
+                      >
+                        x
+                      </button>
+                    </div>
+                  )}
                 </Card>
               </Grid>
             ))}
